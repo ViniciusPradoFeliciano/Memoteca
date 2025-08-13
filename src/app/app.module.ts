9c4253bc589466d36b4b412474419c3d47b2efd6
Vinicius Feliciano <viniciuspradofeliciano@gmail.com>
2025-08-12 21:08:19 -0300

    chore: criando as pastas de rodape e criar-pensamento e configurando o forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamentos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentosComponent
   ],
   imports: [
     BrowserModule,
